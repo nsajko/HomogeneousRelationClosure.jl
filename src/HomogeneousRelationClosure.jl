e8182826afe6b3d0fc5237bb0aacff6f3620b2d7
Neven Sajko <4944410+nsajko@users.noreply.github.com>
2025-10-02 18:40:12 +0200

    implement: symmetric (#9)

--- src/HomogeneousRelationClosure.jl
+++ src/HomogeneousRelationClosure.jl
@@ -1,5 +1,7 @@
 module HomogeneousRelationClosure
     export
+        homogeneous_relation_symmetric_closure!,
+        homogeneous_relation_symmetric_reduction!,
         homogeneous_relation_reflexive_closure!,
         homogeneous_relation_reflexive_reduction!
     function square_matrix_axis(a::AbstractMatrix)
@@ -26,5 +28,33 @@ module HomogeneousRelationClosure
         mutate_diagonal!(zero, a)
     end
     # TODO: transitive
-    # TODO: symmetric
+    function homogeneous_relation_symmetric_closure!(a::AbstractMatrix)
+        axis = square_matrix_axis(a)
+        for I ∈ eachindex(axis)
+            i = axis[I]
+            for J ∈ (I + 1):lastindex(axis)
+                j = axis[J]
+                x_ij = a[i, j]
+                x_ji = a[j, i]
+                y = x_ij + x_ji
+                a[i, j] = y
+                a[j, i] = y
+            end
+        end
+        a
+    end
+    function homogeneous_relation_symmetric_reduction!(a::AbstractMatrix)
+        axis = square_matrix_axis(a)
+        for I ∈ eachindex(axis)
+            i = axis[I]
+            for J ∈ (I + 1):lastindex(axis)
+                j = axis[J]
+                x_ij = a[i, j]
+                x_ji = a[j, i]
+                y_ji = (!x_ij) * x_ji
+                a[j, i] = y_ji
+            end
+        end
+        a
+    end
 end
