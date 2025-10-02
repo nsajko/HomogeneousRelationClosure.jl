c8bac89a2e2232829162453c7e8dd12c15d31f27
Neven Sajko <s@purelymail.com>
2025-10-02 02:05:14 +0200

    test: move Aqua.jl to the end

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,12 @@
 using HomogeneousRelationClosure
 using Test
-using Aqua
 
 @testset "HomogeneousRelationClosure.jl" begin
-    @testset "Code quality (Aqua.jl)" begin
-        Aqua.test_all(HomogeneousRelationClosure)
-    end
     # Write your tests here.
+end
+
+using Aqua: Aqua
+
+@testset "Code quality (Aqua.jl)" begin
+    Aqua.test_all(HomogeneousRelationClosure)
 end
