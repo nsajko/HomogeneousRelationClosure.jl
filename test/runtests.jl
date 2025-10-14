460ad6beb46650195feddfed0a9559cd99ec2695
Neven Sajko <4944410+nsajko@users.noreply.github.com>
2025-10-14 23:22:39 +0200

    include the Boolean algebra package under `test/`, test with v1.10 (#22)
    Fixes #8

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,5 @@
 using HomogeneousRelationClosure
 using Test
-using TwoElementBooleanAlgebra: Boole
 
 module ExampleAlgebraicStructureTypes
     struct ExampleAlgebraicStructureType
@@ -34,6 +33,60 @@ module ExampleAlgebraicStructureTypes
         l.x < r.x
     end
 end
+
+module Booles
+    export Boole
+    """
+        Boole
+
+    Simplest non-trivial Boolean algebra, the two-element Boolean algebra.
+
+    Subtypes `Integer`.
+
+    Different from `Bool` in that `one(Boole) + one(Boole)` equals one instead of two.
+    """
+    struct Boole <: Integer
+        b::Bool
+        function Boole(b::Bool)
+            new(b)
+        end
+    end
+    function Boole(b::Int)
+        Boole(Bool(b))
+    end
+    function Base.Bool(b::Boole)
+        b.b
+    end
+    function Base.Int(b::Boole)
+        Int(Bool(b))
+    end
+    function Base.:(+)(l::Boole, r::Boole)
+        Boole(l.b | r.b)
+    end
+    function Base.:(*)(l::Boole, r::Boole)
+        Boole(l.b & r.b)
+    end
+    function Base.:(<)(l::Boole, r::Boole)
+        l.b < r.b
+    end
+    function Base.typemin(::Type{Boole})
+        zero(Boole)
+    end
+    function Base.typemax(::Type{Boole})
+        one(Boole)
+    end
+    function Base.:(!)(b::Boole)
+        Boole(!(b.b))
+    end
+    function Base.show(io::IO, b::Boole)
+        show(io, Boole)
+        print(io, '(')
+        show(io, Int(b))
+        print(io, ')')
+    end
+end
+
+using .Booles
 
 function equal_vectors(l::AbstractVector, r::AbstractVector)
     if l != r
